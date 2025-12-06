7fa6bb5941d527097e574ab6a3bdb5a48f4be102
Clément <clemm.favre@gmail.com>
2025-12-07 00:48:21 +0100

    small fix: clear was not removing the file

--- public/script.js
+++ public/script.js
@@ -78,6 +78,7 @@ async function sendFileToBackend(file) {
 clearButton.addEventListener("click", () => {
     ldrContent = "";
     dropZone.textContent = dropZoneText;
+    ldr_file = "";
 })
 
 convertButton.addEventListener("click", () => {
